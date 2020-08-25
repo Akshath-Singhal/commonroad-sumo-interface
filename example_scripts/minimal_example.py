26c1e9b45d4ab93f548d6ca5e98b247adae4918c
Akshath Singhal <singhal.akshath97@gmail.com>
2020-08-25 09:27:41 +0530

    minimal_script changed

--- example_scripts/minimal_example.py
+++ example_scripts/minimal_example.py
@@ -3,7 +3,8 @@ Minimal example, plugging in of trajectory planner required.
 """
 from sumo2cr.interface.sumo_simulation import SumoSimulation
 from sumo2cr.visualization.video import create_video
-from scenarios.a9.scenario_config import Conf as conf
+from scenarios.tri_junction.scenario_config import Conf as conf
+from copy import deepcopy
 
 __author__ = "Moritz Klischat"
 __copyright__ = "TUM Cyber-Physical Systems Group"
@@ -16,8 +17,8 @@ __status__ = "Released"
 sumo_sim = SumoSimulation()
 sumo_sim.initialize(conf)
 
-output_folder = "/path/to/output_folder"
-
+output_folder = "/home/lab/commonroad_output"
+ego_trajectory = []
 for t in range(conf.simulation_steps):
     ego_vehicles = sumo_sim.ego_vehicles
     commonroad_scenario = sumo_sim.commonroad_scenario_at_time_step(sumo_sim.current_time_step)
@@ -25,7 +26,9 @@ for t in range(conf.simulation_steps):
     # plan trajectories for all ego vehicles
     for id, ego_vehicle in ego_vehicles.items():
         current_state = ego_vehicle.current_state
-        ego_trajectory =  # plug in a trajectory planner here
+        next_state = deepcopy(current_state)
+        next_state.time_step = sumo_sim.current_time_step +1
+        ego_trajectory.append(next_state) #Implement path planning
         ego_vehicle.set_planned_trajectory(ego_trajectory)
 
     sumo_sim.simulate_step()
@@ -33,4 +36,3 @@ for t in range(conf.simulation_steps):
 sumo_sim.stop()
 
 create_video(sumo_sim, conf.video_start, conf.video_end, output_folder)
-
